17fdd4041b72b4897cda70450368a7dfba89a051
Adequin Renaud <adequin.renaud@gmail.com>
2016-03-21 06:42:55 +0100

    dico valué :-)

--- Code/grille/Dictionnaire.py
+++ Code/grille/Dictionnaire.py
@@ -1,14 +1,16 @@
+import random
 import time
 import re
 from codecs import *
 
 class Noeud:
 
-    def __init__(self, peut_finir=False):
+    def __init__(self, peut_finir=False, value=0):
         self.peut_finir = peut_finir
         self.liste_Noeud = dict()
+        self.value = value
 
-    def add_Fils(self, c, peut_finir):
+    def add_Fils(self, c, peut_finir, value=0):
         if c in self.liste_Noeud:
             self.liste_Noeud[c].setPeut_finir(peut_finir)
         else:
@@ -18,26 +20,35 @@ class Noeud:
     def getFils(self, c):
         return self.liste_Noeud[c]
 
-    def setPeut_finir(self, peut_finir):
+    def setPeut_finir(self, peut_finir, value=0):
         self.peut_finir |= peut_finir
+        self.value = value
 
-    def get_list_Mots(self, pattern, mot, profondeur):
+    def get_list_Mots(self, pattern, mot, profondeur, getValue=False):
         liste = set()
         if profondeur == len(pattern)-1:
             if pattern[profondeur] in '.':
                 for c, n in self.liste_Noeud.items():
                     if n.peut_finir:
-                        liste.add(mot + c)
+                        if getValue:
+                            liste.add((mot + c, n.value))
+                        else:
+                            liste.add(mot + c)
             else:
                 if pattern[profondeur] in self.liste_Noeud and self.liste_Noeud[pattern[profondeur]].peut_finir:
-                    liste.add(mot + pattern[profondeur])
+                    print self.value
+                    if getValue:
+                        liste.add((mot + pattern[profondeur], self.liste_Noeud[pattern[profondeur]].value))
+                    else:
+                        liste.add(mot + pattern[profondeur])
+
         else:
             if pattern[profondeur] in '.':
                 for c, n in self.liste_Noeud.items():
-                    liste.update(n.get_list_Mots(pattern, mot+c, profondeur+1))
+                    liste.update(n.get_list_Mots(pattern, mot+c, profondeur+1, getValue))
             else:
                 if pattern[profondeur] in self.liste_Noeud:
-                    liste.update(self.liste_Noeud[pattern[profondeur]].get_list_Mots(pattern, mot+pattern[profondeur], profondeur+1))
+                    liste.update(self.liste_Noeud[pattern[profondeur]].get_list_Mots(pattern, mot+pattern[profondeur], profondeur+1, getValue))
         return liste
 
     def getAllLetter(self,lettres, profondeur):
@@ -74,7 +85,8 @@ class Noeud:
 
 
 class Dico:
-    def __init__(self, file=None, liste=None):
+
+    def __init__(self, file=None, liste=None, value=False):
         self.lettres = dict()
         lines = None
         if file:
@@ -84,14 +96,17 @@ class Dico:
         for line in lines:
             #line = line.decode('ISO-8859-1').encode('utf8')
             line = line.rstrip()
-            self.add_Mot(line)
+            v = random.random()
 
-    def add_Mot(self, mot):
+            self.add_Mot(line, value=v)
+
+    def add_Mot(self, mot, value=0):
+        print value
         mot = mot.upper()
         n  = self.get_Lettre(mot[0])
         for l in mot[1:]:
             n = n.add_Fils(l, False)
-        n.setPeut_finir(True)
+        n.setPeut_finir(True, value)
 
     def get_Lettre(self, c, peut_finir=False):
         if c in self.lettres:
@@ -99,15 +114,15 @@ class Dico:
         self.lettres[c] = Noeud(peut_finir)
         return self.lettres[c]
 
-    def get_New_Domaine(self, mot_Imcomplet):
+    def get_New_Domaine(self, mot_Imcomplet, getValue=False):
         mot_Imcomplet = mot_Imcomplet.upper()
         liste = set()
         if (mot_Imcomplet[0] in '.'):
             for c, n in self.lettres.items():
-                liste.update(n.get_list_Mots(mot_Imcomplet, c, 1))
+                liste.update(n.get_list_Mots(mot_Imcomplet, c, 1, getValue))
         else:
             if mot_Imcomplet[0] in self.lettres:
-                liste.update(self.lettres[mot_Imcomplet[0]].get_list_Mots(mot_Imcomplet, mot_Imcomplet[0], 1))
+                liste.update(self.lettres[mot_Imcomplet[0]].get_list_Mots(mot_Imcomplet, mot_Imcomplet[0], 1, getValue))
         return liste
 
     def removeMot(self, mot):
@@ -117,9 +132,8 @@ class Dico:
         else:
             self.lettres[mot[0]].removeMot(mot[1:])
 
-
-    def get_Domaine(self, taille):
-        return self.get_New_Domaine(''.join(['.' for i in range(taille)]))
+    def get_Domaine(self, taille, getValue=False):
+        return self.get_New_Domaine(''.join(['.' for i in range(taille)]), getValue)
 
     def getAllLettre(self,profondeur):
         lettres = set()
@@ -155,11 +169,11 @@ if __name__ == "__main__":
     #start_time = time.time()
     #
     d = Dico(liste=file)
-    print d.get_Domaine(4)
+    print d.get_Domaine(4, getValue=True)
     print d.getAllLettre(3)
     d.updateFromContraintes(3, ["B", "C"])
     print d.getAllLettre(3)
-    print d.get_Domaine(4)
+    print d.get_Domaine(4, getValue=True)
     #elapsed_time = time.time() - start_time
     #print("creation dictionnaire " + file + " : " + str(elapsed_time))
     #print ""
