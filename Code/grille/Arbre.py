3a87fb8d8ff00130000a11831eea0a8b16b73040
Adequin Renaud <adequin.renaud@gmail.com>
2016-03-31 21:07:48 +0200

    bed & breakfast :-)

--- Code/grille/Arbre.py
+++ Code/grille/Arbre.py
@@ -13,35 +13,44 @@ class Noeud:
     def __init__(self, pere, motListe, motObj, couple, prof=1):
         self.pere = pere
         self.fils = []
-        self.listeMot = motListe
+        self.listeMot = motListe[:]
         self.motObj = motObj
         self.mot = couple[0]
         if self.pere:
             self.value = min(couple[1], self.pere.value)
-            self.listeMotsAttribue = self.pere.listeMotsAttribue + [(motObj, couple[0])]
+            self.listeMotsAttribue = self.pere.listeMotsAttribue + [(self.pere.motObj, self.pere.mot)]
         else:
             self.value = couple[1]
-            self.listeMotsAttribue = [(motObj, couple[0])]
+            self.listeMotsAttribue = []
         self.prof = prof
 
     def getValue(self):
         return self.value
         
     def create_Fils(self, algo):
+        print len(self.listeMot)
         if not self.listeMot:
             # si la liste est vide ce noeud est la meilleur solution            
             return self
-            
-        m = self.listeMot[0]
 
+        xk = algo.heur(self.listeMot, None)
+        self.listeMot.remove(xk)
+        #m = self.listeMot[0]
+        '''
         for mot in m.getValueDomaine():
             if not mot in [self.listeMotsAttribue[i][0] for i in range(len(self.listeMotsAttribue))]:
                 consist = True
                 for motAtt in self.listeMotsAttribue:
                     if not algo.consistance((motAtt[0], motAtt[1]), (m, mot) ):
+                        consist = False
                         break
                 if consist:
                     self.fils += [Noeud(self, self.listeMot[1:], m, mot, self.prof+1)]
+        '''
+        for mot in xk.getValueDomaine():
+            if len(algo.consistante(self.listeMotsAttribue, (self.motObj, self.mot))) == 0:
+                self.fils += [Noeud(self, self.listeMot[:], xk, mot, self.prof+1)]
+
         return self.fils
         
 
@@ -53,6 +62,7 @@ class Arbre:
         mot = motListe.pop(0)
         for el in mot.getValueDomaine():
             self.listeNoeud += [Noeud(None, motListe, mot, el)]
+        self.listeNoeud = sorted(self.listeNoeud, key=lambda x: x.value, reverse=True)
         self.algo = algo
         #sorted(self.listeNoeud, key=lambda value: (value[1]), reverse=True)
 
@@ -70,6 +80,7 @@ class Arbre:
         #pos = random.choice(posMax)
 
         # Si plusieur Noeud son max peut-être utiliser une heuristique
+        self.listeNoeud = sorted(self.listeNoeud, key=lambda x: x.value, reverse=True)
         return self.listeNoeud.pop(0)
 
 
@@ -87,5 +98,5 @@ class Arbre:
         else:
             # sinon c'est une liste de Noeud
             self.listeNoeud += l
+            #print (len(self.listeNoeud))
 
-            #sorted(self.listeNoeud, key=lambda value: (value[1]), reverse=True)
